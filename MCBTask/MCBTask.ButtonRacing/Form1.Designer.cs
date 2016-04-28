c349ce90d4a088ecc5ff09ebd33b65555f7c3b0a
i.akhaltsev <i.akhaltsev@pflb.ru>
2016-04-29 00:29:32 +0300

    Buttons' Racing

---
+++ MCBTask/MCBTask.ButtonRacing/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace MCBTask.ButtonRacing
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Racer0 = new System.Windows.Forms.Button();
+            this.Racer1 = new System.Windows.Forms.Button();
+            this.Racer2 = new System.Windows.Forms.Button();
+            this.Racer3 = new System.Windows.Forms.Button();
+            this.StartRaceButon = new System.Windows.Forms.Button();
+            this.ResetRaceButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Racer0
+            // 
+            this.Racer0.Location = new System.Drawing.Point(12, 12);
+            this.Racer0.Name = "Racer0";
+            this.Racer0.Size = new System.Drawing.Size(55, 55);
+            this.Racer0.TabIndex = 0;
+            this.Racer0.UseVisualStyleBackColor = true;
+            // 
+            // Racer1
+            // 
+            this.Racer1.Location = new System.Drawing.Point(12, 73);
+            this.Racer1.Name = "Racer1";
+            this.Racer1.Size = new System.Drawing.Size(55, 55);
+            this.Racer1.TabIndex = 1;
+            this.Racer1.UseVisualStyleBackColor = true;
+            // 
+            // Racer2
+            // 
+            this.Racer2.Location = new System.Drawing.Point(12, 134);
+            this.Racer2.Name = "Racer2";
+            this.Racer2.Size = new System.Drawing.Size(55, 55);
+            this.Racer2.TabIndex = 2;
+            this.Racer2.UseVisualStyleBackColor = true;
+            // 
+            // Racer3
+            // 
+            this.Racer3.Location = new System.Drawing.Point(12, 195);
+            this.Racer3.Name = "Racer3";
+            this.Racer3.Size = new System.Drawing.Size(55, 55);
+            this.Racer3.TabIndex = 3;
+            this.Racer3.UseVisualStyleBackColor = true;
+            // 
+            // StartRaceButon
+            // 
+            this.StartRaceButon.Location = new System.Drawing.Point(12, 295);
+            this.StartRaceButon.Name = "StartRaceButon";
+            this.StartRaceButon.Size = new System.Drawing.Size(75, 23);
+            this.StartRaceButon.TabIndex = 4;
+            this.StartRaceButon.Text = "Старт";
+            this.StartRaceButon.UseVisualStyleBackColor = true;
+            this.StartRaceButon.Click += new System.EventHandler(this.StartRaceButon_Click);
+            // 
+            // ResetRaceButton
+            // 
+            this.ResetRaceButton.Location = new System.Drawing.Point(93, 295);
+            this.ResetRaceButton.Name = "ResetRaceButton";
+            this.ResetRaceButton.Size = new System.Drawing.Size(75, 23);
+            this.ResetRaceButton.TabIndex = 5;
+            this.ResetRaceButton.Text = "Сброс";
+            this.ResetRaceButton.UseVisualStyleBackColor = true;
+            this.ResetRaceButton.Click += new System.EventHandler(this.ResetRaceButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(761, 357);
+            this.Controls.Add(this.ResetRaceButton);
+            this.Controls.Add(this.StartRaceButon);
+            this.Controls.Add(this.Racer3);
+            this.Controls.Add(this.Racer2);
+            this.Controls.Add(this.Racer1);
+            this.Controls.Add(this.Racer0);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Racer0;
+        private System.Windows.Forms.Button Racer1;
+        private System.Windows.Forms.Button Racer2;
+        private System.Windows.Forms.Button Racer3;
+        private System.Windows.Forms.Button StartRaceButon;
+        private System.Windows.Forms.Button ResetRaceButton;
+    }
+}
+
---
+++ MCBTask/MCBTask.ButtonRacing/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MCBTask.ButtonRacing
+{
+    public partial class Form1 : Form
+    {
+        private CancellationTokenSource _cts;
+        private CancellationToken _token;
+        public Form1()
+        {
+            InitializeComponent();
+            _cts = new CancellationTokenSource();
+            _token = _cts.Token;
+        }
+
+        private void StartRaceButon_Click(object sender, EventArgs e)
+        {
+            var task0 = Task.Factory.StartNew(() => GoTask(Racer0), _token);
+            var task1 = Task.Factory.StartNew(() => GoTask(Racer1), _token);
+            var task2 = Task.Factory.StartNew(() => GoTask(Racer2), _token);
+            var task3 = Task.Factory.StartNew(() => GoTask(Racer3), _token);
+        }
+
+        private void GoTask(Control button)
+        {
+            while (button.Location.X + button.Width < Width)
+            {
+                var rand = new Random();
+                button.Invoke((Action)(() =>
+                {
+                    var rnd = rand.Next(1, 50) / 4;
+                    Debug.WriteLine(button.Name + ":" + rnd);
+                    button.Location = new Point(button.Location.X + rnd, button.Location.Y);
+                }));
+                Thread.CurrentThread.Join(70);
+            }
+        }
+        private void Go(Control button)
+        {
+            ThreadPool.QueueUserWorkItem(sender =>
+            {
+
+                var btn = (Button)sender;
+                while (btn.Location.X + btn.Width < Width && !_cts.Token.IsCancellationRequested)
+                {
+                    var rand = new Random();
+                    btn.Invoke((Action)(() =>
+                    {
+                        var rnd = rand.Next(1, 50) / 5;
+                        Debug.WriteLine(button.Name + ":" + rnd);
+                        btn.Location = new Point(btn.Location.X + rnd, btn.Location.Y);
+                    }));
+                    Thread.CurrentThread.Join(70);
+                }
+            }, button);
+        }
+
+        private void ResetRaceButton_Click(object sender, EventArgs e)
+        {
+            _cts.Cancel();
+            Racer0.Location = new Point(12, Racer0.Location.Y);
+            Racer1.Location = new Point(12, Racer1.Location.Y);
+            Racer2.Location = new Point(12, Racer2.Location.Y);
+            Racer3.Location = new Point(12, Racer3.Location.Y);
+        }
+
+
+        private static void Reset(Button button)
+        {
+            ThreadPool.QueueUserWorkItem(sender =>
+            {
+                var btn = (Button)sender;
+                btn.Invoke((Action)(() =>
+                {
+                    btn.Location = new Point(12, btn.Location.Y);
+                }));
+            }, button);
+        }
+    }
+}
